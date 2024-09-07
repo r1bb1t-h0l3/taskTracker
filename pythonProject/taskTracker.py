5fd3ba61a3c4a95673e4021f23dce68a4b0b74ef
sasha <arefieva.aleksandra@gmail.com>
2024-09-07 20:10:40 +0200

    read/write DB from disk

--- pythonProject/taskTracker.py
+++ pythonProject/taskTracker.py
@@ -1,9 +1,37 @@
 #!/usr/bin/python3
 
 import sys
+import json
 
-def add():
-    print("add")
+DATABASE_NAME = "taskDB.json"
+NAME_KEY = "name"
+DONE_KEY = "isDone"
+TASKS_KEY = "allTasks"
+
+
+def loadTasks():
+    try:
+        with open(DATABASE_NAME, 'r') as file:
+            data = json.load(file)
+
+        return data[TASKS_KEY]
+    except:
+        return []
+
+
+def saveTasks(tasks):
+    tasksRecord = {
+        TASKS_KEY: tasks
+    }
+    with open(DATABASE_NAME, 'w') as file:
+        json.dump(tasksRecord, file, indent=4)
+
+
+def add(tasks):
+    tasks.append({
+        NAME_KEY: 'my first task',
+        DONE_KEY: False
+    })
 
 
 def update():
@@ -31,9 +59,10 @@ def main():
         print("invalid arguments")
         exit(1)
 
+    tasks = loadTasks()
     action = sys.argv[1]
     if action == "add":
-        add()
+        add(tasks)
     elif action == "update":
         update()
     elif action == "delete":
@@ -47,7 +76,7 @@ def main():
     else:
         print("unknown action")
         exit(2)
-
+    saveTasks(tasks)
 
 if __name__ == "__main__":
     main()
